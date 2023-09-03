132908ae7249a2e2afb5fbd7d6a18594c0c14cb1
Vishesh <vishesht27@gmail.com>
2023-09-03 11:23:53 +0530

    Added Recommendations

--- app.py
+++ app.py
@@ -1,16 +1,72 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from datetime import date
 import requests
-from textblob import TextBlob
 import os
 from dotenv import load_dotenv
 import base64
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
 
 load_dotenv()
 
 app = Flask(__name__)
 
 api_key = os.environ.get("DROPBOX_API_KEY")
+AZURE_TEXT_ANALYTICS_ENDPOINT = os.environ.get("AZURE_TEXT_ANALYTICS_ENDPOINT")
+AZURE_TEXT_ANALYTICS_KEY = os.environ.get("AZURE_TEXT_ANALYTICS_KEY")
+
+
+def extract_key_phrases(text):
+    endpoint = f"{AZURE_TEXT_ANALYTICS_ENDPOINT}text/analytics/v3.1-preview.4/keyPhrases"
+    headers = {
+        "Ocp-Apim-Subscription-Key": AZURE_TEXT_ANALYTICS_KEY,
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    }
+    data = {
+        "documents": [
+            {
+                "id": "1",
+                "text": text
+            }
+        ]
+    }
+    
+    response = requests.post(endpoint, headers=headers, json=data)
+    if response.status_code == 200:
+        phrases = response.json()['documents'][0]['keyPhrases']
+        return phrases
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+        return []
+
+
+STANDARD_CLAUSES = {
+    "employment": ["non-compete", "confidentiality", "benefits", "termination"],
+    "rental": ["maintenance", "deposit", "termination", "renewal"],
+    "nda": ["disclosure", "duration", "penalties"]
+}
+
+def generate_recommendations(contract_type, key_phrases):
+    missing_clauses = [clause for clause in STANDARD_CLAUSES[contract_type] if clause not in key_phrases]
+    recommendations = []
+    for clause in missing_clauses:
+        recommendations.append(f"Consider adding a {clause} clause.")
+    return recommendations
+
+
+
+
+
+def authenticate_client():
+    ta_credential = AzureKeyCredential(AZURE_TEXT_ANALYTICS_KEY)
+    text_analytics_client = TextAnalyticsClient(
+        endpoint=AZURE_TEXT_ANALYTICS_ENDPOINT, credential=ta_credential
+    )
+    return text_analytics_client
+
+
+client = authenticate_client()
 
 
 def send_to_dropbox_sign(contract_content, api_key):
@@ -68,11 +124,12 @@ def generate_contract():
         return render_template("employment_form.html")
 
 
-def analyze_sentiment(clause):
-    analysis = TextBlob(clause)
-    if analysis.sentiment.polarity > 0:
+def analyze_sentiment_with_azure(clause):
+    response = client.analyze_sentiment(documents=[clause])[0]
+    sentiment = response.sentiment
+    if sentiment == "positive":
         return "Positive tone detected in the custom clause."
-    elif analysis.sentiment.polarity == 0:
+    elif sentiment == "neutral":
         return "Neutral tone detected in the custom clause."
     else:
         return "Negative tone detected in the custom clause."
@@ -82,7 +139,7 @@ def analyze_sentiment(clause):
 def finalize_contract():
     contract_type = request.form.get("contract_type", "")
     custom_clause = request.form.get("custom_clause", "")
-    sentiment_feedback = analyze_sentiment(custom_clause)
+    sentiment_feedback = analyze_sentiment_with_azure(custom_clause)
 
     if contract_type == "rental":
         landlord_name = request.form.get("landlord_name", "")
@@ -142,14 +199,22 @@ def finalize_contract():
     </form>
     """
 
-    return render_template("output_template.html", content=contract_display)
+    # Extract key phrases
+    key_phrases = extract_key_phrases(contract)
+
+    # Generate recommendations
+    recommendations = generate_recommendations(contract_type, key_phrases)
+
+    
+    return render_template("output_template.html", content=contract_display, recommendations=recommendations)
+
 
 
 @app.route("/send_to_dropbox", methods=["POST"])
 def send_to_dropbox():
     contract_content = request.form["contract_content"]
     message = send_to_dropbox_sign(contract_content, api_key)
-    return render_template('success_notification.html', message=message)
+    return render_template("success_notification.html", message=message)
 
 
 if __name__ == "__main__":
